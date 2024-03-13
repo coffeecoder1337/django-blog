5f8aed83ce62a2f3c1cba248bba2b26da5f66f33
coffeecoder1337 <krox1337@yandex.ru>
2024-03-13 20:57:50 +0300

    dockerized

--- testsite/settings.py
+++ testsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fusnv3#vkjkwpyr)rv5+)k%y0x#mx$$)!w69e(y2(+ajnm(vh0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0']
 INTERNAL_IPS = ['127.0.0.1',]
 
 
